7a1209bfe7f94627ba73ebb370971adcbb4425f4
Ansh Chaurasia <achaurasia@Anshs-MacBook-Air.local>
2020-03-08 00:41:29 -0800

    add stats

--- gesture-by-angle-classifier/output.py
+++ gesture-by-angle-classifier/output.py
@@ -6,6 +6,7 @@ from models import GestureAngleClassifier
 
 def display_image(path, pred):
     plt.imshow(mimg.imread(path))
+    plt.text(0,0,'Classification: ', pred[0], ' and similarity: ', pred[1])
     plt.pause(0.5)
     plt.show()
 
@@ -14,9 +15,6 @@ def display(input_folder):
     model = GestureAngleClassifier('train_angles.npy', 'train_labels.npy')
 
     for path in os.listdir(input_folder):
-        pred = model.get_predictions(input_folder + '/' + path)
-        pred = pred[0]
-
-        display_image('./similarity_output/openpose_result.jpg', pred)
+        display_image('./similarity_output/openpose_result.jpg', model.get_predictions(input_folder + '/' + path))
 
 display('./shot2')
