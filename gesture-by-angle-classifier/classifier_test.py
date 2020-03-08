131256bff95381dd492cfaac0ee1da3ea3eeaf01
Ansh Chaurasia <achaurasia@Anshs-MacBook-Air.local>
2020-03-07 23:57:50 -0800

    test

--- gesture-by-angle-classifier/classifier_test.py
+++ gesture-by-angle-classifier/classifier_test.py
@@ -2,3 +2,6 @@ from models import GestureAngleClassifier
 
 model = GestureAngleClassifier('train_angles.npy', 'train_labels.npy')
 print('Model Sucessfully Built.')
+
+pred = model.get_predictions('../gesture-by-feature-classifier/Dataset/Backhand/Image-5537.jpg')
+print(pred[1])
