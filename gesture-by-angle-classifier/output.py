0bee03707d6bf5fc1628da576597e22937ee1925
Ansh Chaurasia <achaurasia@Anshs-MacBook-Air.local>
2020-03-08 00:48:02 -0800

    add multiple functionality

--- gesture-by-angle-classifier/output.py
+++ gesture-by-angle-classifier/output.py
@@ -6,13 +6,14 @@ from models import GestureAngleClassifier
 
 def display_image(path, pred):
     plt.imshow(mimg.imread(path))
-    plt.text(0,0,'Classification: ' + pred[0] + ' and similarity: ' + str(pred[1]))
-    plt.pause(0.5)
-    plt.show()
+    plt.text(0,0,pred[0] + ' Similarity: ' + str(pred[1]))
+    plt.pause(0.1)
+    plt.draw()
 
 
 def display(input_folder):
     model = GestureAngleClassifier('train_angles.npy', 'train_labels.npy')
+    plt.show()
 
     for path in os.listdir(input_folder):
         display_image('./similarity_output/openpose_result.jpg', model.get_predictions(input_folder + '/' + path))
