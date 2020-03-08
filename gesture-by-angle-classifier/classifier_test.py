23b6aee70ac7cf4059c9388fbe4db751995e3de8
Ansh Chaurasia <achaurasia@Anshs-MacBook-Air.local>
2020-03-07 23:59:27 -0800

    test label

--- gesture-by-angle-classifier/classifier_test.py
+++ gesture-by-angle-classifier/classifier_test.py
@@ -4,4 +4,4 @@ model = GestureAngleClassifier('train_angles.npy', 'train_labels.npy')
 print('Model Sucessfully Built.')
 
 pred = model.get_predictions('../gesture-by-feature-classifier/Dataset/Backhand/Image-5537.jpg')
-print(pred[1])
+print(pred[0])
