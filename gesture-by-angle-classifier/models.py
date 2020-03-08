79a6ed274e51d9fd158d0dd2330c7f393815ab96
Ansh Chaurasia <achaurasia@Anshs-MacBook-Air.local>
2020-03-07 23:12:07 -0800

    oof

--- gesture-by-angle-classifier/models.py
+++ gesture-by-angle-classifier/models.py
@@ -23,7 +23,7 @@ class GestureAngleClassifier:
 
 
     classifier = Sequential([
-            Flatten(input_shape = (,SIZE)) ,
+            Flatten(input_shape = (1,SIZE)) ,
             Dense(128, activation='relu')   ,
             Dense(len(output_labels), activation='softmax')
         ])
