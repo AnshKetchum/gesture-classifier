6fef08b1c33218a7925c3b5b132a28cff04445e5
Ansh Chaurasia <achaurasia@Anshs-MacBook-Air.local>
2020-03-08 00:23:29 -0800

    added similarity

---
+++ gesture-by-angle-classifier/output.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt 
+import matplotlib.image  as mimg
+import os
+import cv2
+from models import GestureAngleClassifier
+
+def display_image(path, pred):
+    plt.imshow(mimg.imread(path))
+    plt.show()
+
+
+def display(input_folder):
+    model = GestureAngleClassifier('train_angles.npy', 'train_labels.npy')
+
+    for path in os.listdir(input_folder):
+        pred = model.get_predictions(input_folder + '/' + path)
+        pred = pred[0]
+
+        display_image('./similarity_output/openpose-result.jpg', pred)
+ 
